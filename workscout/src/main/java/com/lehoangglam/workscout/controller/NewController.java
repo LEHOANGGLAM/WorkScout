54521f31287ad0edb16b9d50f213907958441472
LEHOANGGLAM <lelam1384@gmail.com>
2022-09-19 02:34:23 +0700

    add header

--- workscout/src/main/java/com/lehoangglam/workscout/controller/NewController.java
+++ workscout/src/main/java/com/lehoangglam/workscout/controller/NewController.java
@@ -3,12 +3,14 @@ package com.lehoangglam.workscout.controller;
 import com.lehoangglam.workscout.entities.New;
 import com.lehoangglam.workscout.repository.NewRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("/api")
 public class NewController {
