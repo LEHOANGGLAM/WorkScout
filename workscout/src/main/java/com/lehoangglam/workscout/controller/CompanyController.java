1116ba4645b08e44a74e1258be64fa0e97bc6d92
LEHOANGGLAM <lelam1384@gmail.com>
2022-09-19 19:36:29 +0700

    add jobpost

--- workscout/src/main/java/com/lehoangglam/workscout/controller/CompanyController.java
+++ workscout/src/main/java/com/lehoangglam/workscout/controller/CompanyController.java
@@ -1,10 +1,10 @@
 package com.lehoangglam.workscout.controller;
 
-
+import com.lehoangglam.workscout.entities.Company;
 import com.lehoangglam.workscout.entities.JobPost;
+import com.lehoangglam.workscout.repository.CompanyRepository;
 import com.lehoangglam.workscout.repository.JobPostRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Sort;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,12 +15,12 @@ import java.util.List;
 @CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("/api")
-public class JobPostController {
+public class CompanyController {
     @Autowired
-    private JobPostRepository jobPostRepository;
+    private CompanyRepository companyRepository;
 
-    @GetMapping("/jobpost")
-    public List<JobPost> getAllJobPosts(){
-        return jobPostRepository.findAll(Sort.by(Sort.Direction.DESC, "id"));
+    @GetMapping("/companies")
+    public List<Company> gettAllCompanies(){
+        return companyRepository.findAll();
     }
 }
