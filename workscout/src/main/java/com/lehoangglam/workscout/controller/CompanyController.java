45b729bc0720d0c7205531c66d422205b2eb88c1
LEHOANGGLAM <lelam1384@gmail.com>
2022-09-20 21:22:47 +0700

    fix

--- workscout/src/main/java/com/lehoangglam/workscout/controller/CompanyController.java
+++ workscout/src/main/java/com/lehoangglam/workscout/controller/CompanyController.java
@@ -2,13 +2,12 @@ package com.lehoangglam.workscout.controller;
 
 import com.lehoangglam.workscout.entities.Company;
 import com.lehoangglam.workscout.entities.JobPost;
+import com.lehoangglam.workscout.exception.ResourceNotFoundException;
 import com.lehoangglam.workscout.repository.CompanyRepository;
 import com.lehoangglam.workscout.repository.JobPostRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -22,5 +21,29 @@ public class CompanyController {
     @GetMapping("/companies")
     public List<Company> gettAllCompanies(){
         return companyRepository.findAll();
+    }
+
+    @GetMapping("/companies/{id}")
+    public ResponseEntity<Company> getCompanyById(@PathVariable Integer id){
+        Company company = companyRepository.findById(id).orElseThrow(()->
+                new ResourceNotFoundException("Company not exist with id: "+ id));
+        return ResponseEntity.ok(company);
+    }
+
+    @PutMapping("/companies/{id}")
+    public ResponseEntity<Company> updateCompany(@PathVariable Integer id,@RequestBody Company companyNew){
+        Company company = companyRepository.findById(id).orElseThrow(()->
+                new ResourceNotFoundException("Company not exist with id: "+ id));
+
+        company.setImage(companyNew.getImage());
+        company.setAboutCompany(companyNew.getAboutCompany());
+        company.setCompanyEmail(companyNew.getCompanyName());
+        company.setCompanyName(companyNew.getCompanyName());
+        company.setCompanySize(companyNew.getCompanySize());
+        company.setCompanyWebsite(companyNew.getCompanyWebsite());
+
+
+        Company updateCompany = companyRepository.save(company);
+        return ResponseEntity.ok(updateCompany);
     }
 }
